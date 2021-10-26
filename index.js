05a9490341ac46143d0ae454df2a5adfe3803194
Pierce <piercedavidraphael@gmail.com>
2021-10-26 11:19:50 -0500

    Version 3 - JS and a little CSS

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const foodUrl = "https://foodish-api.herokuapp.com/api/"
+const randomFoodUrl = "https://foodish-api.herokuapp.com/api/"
+// Food Urls
+const baseUrl = "https://foodish-api.herokuapp.com/images/"
+
+const biryaniUrl = 'biryani/biryani' 
+const burgerUrl = 'burger/burger'
+const butter_chickenUrl = 'butter-chicken/butter-chicken'
+const dessertUrl = 'dessert/dessert'
+const dosaUrl = 'dosa/dosa'
+const idlyUrl = 'idly/idly'
+const pastaUrl = 'pasta/pasta'
+const pizzaUrl = 'pizza/pizza'
+const riceUrl = 'rice/rice'
+const samosaUrl = 'samosa/samosa'
+
+
+const randomNum = (Math.floor(Math.random()* 20) + 1)
+const jpg = '.jpg'
+
+
+
 const foodMenu = document.getElementById('food-list');
+const bottomMenu = document.getElementById('bottom-list');
+
 
 document.addEventListener('DOMContentLoaded' , fetchImage)
 
 function fetchImage () {
-    for (let i = 0; i < 3; i++) {
-        fetch(foodUrl)
+    for (let i = 0; i < 5; i++) {
+        fetch(randomFoodUrl)
         .then(resp => resp.json())
         .then(foodImg => appendToFoodMenu(foodImg))
     }
-    
 }
+
 
 function appendToFoodMenu (food) {
         const imgOfFood = document.createElement('img');
@@ -18,5 +40,29 @@ function appendToFoodMenu (food) {
         foodMenu.append(imgOfFood);
         imgOfFood.addEventListener('click', (e) => {
             e.preventDefault()
+
         })
     }
+
+
+function appendToSideMenu () {
+    const arrayOfFoodNames = [
+        biryaniUrl, 
+        burgerUrl, 
+        butter_chickenUrl, 
+        dessertUrl, 
+        dosaUrl, 
+        idlyUrl, 
+        pastaUrl, 
+        pizzaUrl,
+        riceUrl, 
+        samosaUrl,
+    ]
+    for (const foodNames of arrayOfFoodNames) {
+        const bottomFood = document.createElement('img');
+        bottomFood.src = (baseUrl + foodNames + randomNum + jpg)
+        bottomMenu.append(bottomFood)
+    }
+}
+
+appendToSideMenu()
