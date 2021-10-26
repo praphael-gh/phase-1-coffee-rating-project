19d5118bed32310a9ac033b086deecba759e7cd7
priscilla <pdjarbeng@outlook.com>
2021-10-26 10:29:48 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const foodUrl = "https://foodish-api.herokuapp.com/api/"
 const foodMenu = document.getElementById('food-list');
 
-const NUMBER_OF_IMAGES = 5
 
 document.addEventListener('DOMContentLoaded' , fetchImage)
 
@@ -9,34 +8,18 @@ function fetchImage () {
     for (let i = 0; i < 3; i++) {
         fetch(foodUrl)
         .then(resp => resp.json())
-        .then(foodImg => appendToArray(foodImg))
+        .then(foodImg => appendToFoodMenu(foodImg))
     }
     
 }
 
-// Get different images from JSON object
-// Assign images to array/Object
 
-function appendToArray (foodObj) {
-    
-}
+function appendToFoodMenu (food) {
+        const imgOfFood = document.createElement('img');
+        imgOfFood.src = food.image;
+        foodMenu.append(imgOfFood);
+        imgOfFood.addEventListener('click', (e) => {
+            e.preventDefault()
+        })
+    }
 
-
-// function appendToFoodMenu (food) {
-//     let i = 0;
-//     while (i < 3) {
-//         const imgOfFood = document.createElement('img');
-//         imgOfFood.src = food.image;
-
-//         foodMenu.append(imgOfFood);
-
-//         imgOfFood.addEventListener('click', (e) => {
-//             e.preventDefault()
-
-//         })
-
-//         i += 1
-
-//     }
-
-// }
